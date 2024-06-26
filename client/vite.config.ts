bd4ed806d7008d4c338d0edefb8e519dc0d5aea7
Yujinndev <xyujinwho@gmail.com>
2024-06-26 22:53:24 +0800

    feat (client): initialize shadcn ui to be a component library

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src/') },
+      {
+        find: '@/components',
+        replacement: path.resolve(__dirname, './src/components/'),
+      },
+      { find: '@/lib', replacement: path.resolve(__dirname, './src/lib/') },
+    ],
+  },
 })
